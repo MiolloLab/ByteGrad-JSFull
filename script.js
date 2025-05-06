fa9f03e45d38639d687824491dcf779787d3e602
MiolloLab <162133896+MiolloLab@users.noreply.github.com>
2025-05-06 11:26:17 -0300

    Criação do JS

---
+++ script.js
@@ -0,0 +1,11 @@
+// VARIABLES (var,let,const)
+const balance = 2000;
+const description = 'Salary';
+const newTransaction = true;
+// In var we can accidentally to orride the variables
+
+const balanceNumberEl = document.querySelector('balance-number');
+balanceNumberEl.textContent = 10000;
+
+balance = 9000;
+
