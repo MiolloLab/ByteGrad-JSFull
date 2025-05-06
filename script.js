aafc77a18358770dba63c8b306425839e3900867
MiolloLab <162133896+MiolloLab@users.noreply.github.com>
2025-05-06 11:29:14 -0300

    jsUpdate01

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const description = 'Salary';
 const newTransaction = true;
 // In var we can accidentally to orride the variables
 
-const balanceNumberEl = document.querySelector('balance-number');
+const balanceNumberEl = document.querySelector('.balance-number');
 balanceNumberEl.textContent = 10000;
 
-balance = 9000;
 
